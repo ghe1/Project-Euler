92f51d33a90c17e77beef9ec20e5748b339a8d2b
ghe1 <ghe1@uchicago.edu>
2014-08-06 10:01:17 -0500

    Implemented non-recursive p2

--- p2.py
+++ p2.py
@@ -1,12 +1,23 @@
-def fib(start1, start2, max, cumsum):
+# Even Fibonacci number sum function without using recursion
+def fib_norecurs(nmax):
+	s, n, cumsum = 0, 1, 0
+	while s < nmax:
+		s, n = n, s + n
+		if n % 2 == 0:
+			cumsum += n
+	return cumsum
+
+# Even Fibonacci number sum function using recursion
+# Would hit recursion limit for higher max
+def fib_recurs(start1, start2, max, cumsum):
 	next = start1 + start2
 	
 	if next > max:
-		print cumsum
-		return
+		return cumsum
 	elif next % 2 == 0:
 		cumsum = cumsum + next
 
-	fib(start2, next, max, cumsum)
+	return fib(start2, next, max, cumsum)
 	
-fib(1, 2, 4000000, 2)
+# print fib_recurs(0, 1, 4000000, 0)
+print fib_norecurs(4000000)
